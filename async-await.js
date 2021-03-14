c3ce3582e1268885a2a5d0dd03d8df9e21496e54
nalendro16 <nalendro_agung@yahoo.com>
2021-03-15 01:50:25 +0700

    async-await http request and throw error

--- async-await.js
+++ async-await.js
@@ -1,12 +1,17 @@
 const getTodo = async () => {
   const request = await fetch('todos/luigi.json')
-  if (request.status === 404) {
-    throw new Error('no data')
+  if (request.status !== 200) {
+    throw new Error('no data fetched')
   }
   const data = await request.json()
   return data
 }
+console.log(1)
+console.log(2)
 
 getTodo()
   .then((data) => console.log(data))
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err.message))
+
+console.log(3)
+console.log(4)
